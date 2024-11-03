44f4995d6edddb0eff79a67a097f03dec29d8ad4
harshdgoel <85487236+harshdgoel@users.noreply.github.com>
2024-11-03 20:59:54 +0530

    Update botController.js

--- src/controllers/botController.js
+++ src/controllers/botController.js
@@ -2,7 +2,7 @@ const { sendResponseToWhatsApp } = require("../services/apiHandler");
 const stateMachine = require("../states/stateMachine");
 
 const handleIncomingMessage = async (phoneNumberId, from, message) => {
-    const responseMessage = stateMachine.handleMessage(from, message);
+    const responseMessage = await stateMachine.transition(message); // Ensure this is awaited
     await sendResponseToWhatsApp(phoneNumberId, from, responseMessage);
 };
 
