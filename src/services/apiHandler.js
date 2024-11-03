4836bff839ebeb7270021ddabf806d67a42fd5ba
harshdgoel <85487236+harshdgoel@users.noreply.github.com>
2024-11-03 17:57:30 +0530

    Update apiHandler.js

--- src/services/apiHandler.js
+++ src/services/apiHandler.js
@@ -1,21 +1,21 @@
 const axios = require("axios");
 const config = require("../config/config");
 
-const sendMessageToWhatsApp = async (phoneNumberId, from, message) => {
+const sendResponseToWhatsApp = async (phoneNumberId, to, message) => {
+    const responseData = {
+        messaging_product: "whatsapp",
+        to: to,
+        text: { body: message }
+    };
+
     try {
-        const response = await axios.post(`https://graph.facebook.com/v17.0/${phoneNumberId}/messages?access_token=${config.whatsappToken}`, {
-            messaging_product: "whatsapp",
-            to: from,
-            text: { body: message }
-        }, {
+        const response = await axios.post(`https://graph.facebook.com/v17.0/${phoneNumberId}/messages?access_token=${config.whatsappToken}`, responseData, {
             headers: { "Content-Type": "application/json" }
         });
-
-        console.log("Message sent:", response.data);
+        console.log("Response sent:", response.data);
     } catch (error) {
-        console.error("Error sending message:", error.response ? error.response.data : error.message);
+        console.error("Error sending response:", error.response ? error.response.data : error.message);
     }
 };
-
 
 module.exports = { sendResponseToWhatsApp };
