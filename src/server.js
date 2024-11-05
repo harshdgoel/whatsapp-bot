c20e7f71dac770e6fab752b9b97988d031c47da5
harshdgoel <85487236+harshdgoel@users.noreply.github.com>
2024-11-05 11:37:19 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,16 +1,34 @@
+const cluster = require("cluster");
 const express = require("express");
 const bodyParser = require("body-parser");
+const os = require("os");
 const botRoutes = require("./routes/botRoutes");
 const config = require("./config/config");
 const errorHandler = require("./middlewares/errorHandler");
 
-const app = express().use(bodyParser.json());
+const numCPUs = os.cpus().length; // Get the number of CPU cores
 
-const PORT = process.env.PORT || 3000;
+if (cluster.isMaster) {
+    console.log(`Master process is running. Spawning ${numCPUs} workers...`);
 
-app.use("/api", botRoutes);
-app.use(errorHandler); // Error handling middleware
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
 
-app.listen(PORT, () => {
-    console.log(`Webhook is listening on port ${PORT}`);
-});
+    cluster.on("exit", (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died`);
+        cluster.fork(); // Replace the worker
+    });
+} else {
+    const app = express();
+
+    app.use(bodyParser.json()); // Body parser middleware
+    app.use("/api", botRoutes); // Routes for your chatbot API
+    app.use(errorHandler); // Error handling middleware
+
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, () => {
+        console.log(`Worker ${process.pid} is listening on port ${PORT}`);
+    });
+}
